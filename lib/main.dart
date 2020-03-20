33955d1e5070bd13d5cead7834e1f74b5759c2b2
Abreu <felipe.abreu@accenture.com>
2020-03-20 12:10:20 -0300

    Se aplicado, este commit irá finalizar a configuração do Firestore e testar a criação de uma coleção e documento no Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home.dart';
 
 void main() {
+
+  //WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: Home(),
       debugShowCheckedModeBanner: false,
     ),
   );
+
+  Firestore.instance
+  .collection("usuarios")
+  .document("001")
+  .setData({"nome" : "Felipe Abreu"});
+
 }
