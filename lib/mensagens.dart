f2ebadc80dc562e13f0cafd5dc6173b7b7aeab83
Abreu <felipe.abreu@accenture.com>
2020-04-03 11:44:27 -0300

    Se aplicado, este commit irá iniciar a construção da interface de mensagens

--- lib/mensagens.dart
+++ lib/mensagens.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model/usuario.dart';
+import 'model/Usuario.dart';
 
 class Mensagens extends StatefulWidget {
   Usuario contato;
@@ -11,13 +11,73 @@ class Mensagens extends StatefulWidget {
 }
 
 class _MensagensState extends State<Mensagens> {
+  TextEditingController _controllerMensagem = TextEditingController();
+
+  _enviarMensagem() {}
+
+  _enviarFoto() {}
+
   @override
   Widget build(BuildContext context) {
+    var caixaMensagem = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                controller: _controllerMensagem,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                    hintText: "Digite uma mensagem...",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                    prefixIcon: IconButton(
+                        icon: Icon(Icons.camera_alt), onPressed: _enviarFoto)),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xff075E54),
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+            mini: true,
+            onPressed: _enviarMensagem,
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.contato.nome),
       ),
-      body: Container(),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/imagens/bg.png"), fit: BoxFit.cover)),
+        child: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Text("listview"),
+              caixaMensagem,
+              //listview
+              //caixa mensagem
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
